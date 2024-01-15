6689f4161b59ca629ed43324a7d591de7a214f46
Senua <chc0089@gmail.com>
2024-01-15 14:11:47 +0100

    Updated sql script

--- cursoscp.sql
+++ cursoscp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-01-2024 a las 18:28:02
+-- Tiempo de generación: 15-01-2024 a las 14:10:52
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -83,7 +83,7 @@ CREATE TABLE `cursos` (
 INSERT INTO `cursos` (`codigo`, `nombre`, `abierto`, `numeroplazas`, `plazoinscripcion`) VALUES
 (1, 'PHP Avanzado', 1, 11, '2024-01-16'),
 (2, 'PHP Básico', 1, 10, '2024-01-22'),
-(3, 'PHP E-commerce', 0, 10, '2024-01-21'),
+(3, 'PHP E-commerce', 1, 10, '2024-01-21'),
 (36, 'Symfony Avanzado', 1, 6, '2024-01-24');
 
 -- --------------------------------------------------------
@@ -105,7 +105,17 @@ CREATE TABLE `resultados` (
 INSERT INTO `resultados` (`dni`, `solicitudes_admitido`, `puntos`) VALUES
 ('09076123R', 1, 13),
 ('09076123R', 1, 13),
-('77359169A', 1, 14);
+('234567890', 1, 9),
+('123456789', 1, 10),
+('77359169A', 1, 14),
+('012345678', 1, 10),
+('987654321', 1, 4),
+('77359169A', 1, 14),
+('09076123R', 1, 13),
+('123456789', 1, 10),
+('012345678', 1, 10),
+('234567890', 1, 9),
+('987654321', 1, 4);
 
 -- --------------------------------------------------------
 
@@ -142,13 +152,14 @@ INSERT INTO `solicitantes` (`dni`, `apellidos`, `nombre`, `contrasena`, `telefon
 ('09076123R', 'Sánchez Romero', 'Manuel', '1234', '666899078', 'falso2@gmail.com', 'F1234567', 1, 1, 'bbbb', 1, 0, NULL, 'activo', '2024-01-13', 'muchas', 13),
 ('123456789', 'Lopez', 'Maria', 'pass123', '987654321', 'maria.lopez@email.com', 'CC123456', 1, 0, NULL, 1, 1, 'Director', 'activo', '2024-01-14', 'IT Specialist', 10),
 ('234567890', 'Rodriguez', 'Pedro', 'pass789', '555666777', 'pedro.rodriguez@email.com', 'CC345678', 0, 1, 'GRP02', 1, 1, 'Secretario', 'activo', '2024-01-14', 'Network Engineer', 9),
-('345678901', 'Martinez', 'Laura', 'passabc', '111222333', 'laura.martinez@email.com', 'CC456789', 1, 0, NULL, 1, 1, 'Jefe de Departamento', 'activo', '2024-01-14', 'Database Administrator', 8),
+('345678901', 'Martinez', 'Laura', 'passabc', '111222333', 'laura.martinez@email.com', 'CC456789', 1, 1, 'aaaa', 1, 1, 'Director', 'activo', '2005-01-03', 'Database Administrator', 8),
 ('456789012', 'Sanchez', 'Ana', 'pass456', '999000111', 'ana.sanchez@email.com', 'CC678901', 0, 1, 'GRP03', 1, 0, NULL, 'activo', '2024-01-14', 'Security Specialist', 9),
 ('567890123', 'Lopez', 'Isabel', 'pass123', '222333444', 'isabel.lopez@email.com', 'CC890123', 1, 0, NULL, 1, 1, 'Secretario', 'activo', '2024-01-14', 'IT Director', 10),
 ('678901234', 'Perez', 'Miguel', 'passabc', '666777888', 'miguel.perez@email.com', 'CC901234', 0, 1, 'GRP04', 0, 1, 'Jefe de Departamento', 'activo', '2024-01-14', 'Software Architect', 4),
 ('77359169A', 'Hidalgo Cobo', 'Cristina', '1234', '670644812', 'chc0089@gmail.com', 'F3708638', 1, 1, 'aaa', 1, 1, 'Director', 'activo', '2009-01-06', 'aaaa', 14),
 ('789012345', 'Garcia', 'Carlos', 'passxyz', '444555666', 'carlos.garcia@email.com', 'CC567890', 1, 0, NULL, 0, 1, 'Executive', 'inactivo', '2024-01-14', 'Project Manager', 4),
 ('890123456', 'Fernandez', 'David', 'passxyz', '777888999', 'david.fernandez@email.com', 'CC789012', 1, 0, NULL, 0, 1, 'Jefe de Estudios', 'activo', '2024-01-14', 'System Administrator', 5),
+('92045507J', 'Martínez Fernández', 'Jesús', '1234', '6677453219', 'jesus@gmail.com', 'F4567091', 1, 1, 'bbb', 1, 1, 'Director', 'activo', '2005-01-11', 'varias', 14),
 ('987654321', 'Gomez', 'Juan', 'pass456', '123456789', 'juan.gomez@email.com', 'CC789012', 0, 1, 'GRP01', 0, 1, 'Jefe de Estudios', 'activo', '2024-01-14', 'Software Engineer', 4);
 
 --
@@ -200,8 +211,14 @@ INSERT INTO `solicitudes` (`id`, `dni`, `codigocurso`, `fechasolicitud`, `admiti
 (31, '09076123R', 2, '2024-01-10', 1),
 (32, '77359169A', 1, '2024-01-14', 1),
 (33, '77359169A', 2, '2024-01-14', 0),
-(34, '09076123R', 36, '2024-01-14', 0),
-(35, '77359169A', 36, '2024-01-11', 0);
+(34, '09076123R', 36, '2024-01-14', 1),
+(35, '77359169A', 36, '2024-01-11', 1),
+(36, '234567890', 36, '2024-01-14', 1),
+(37, '92045507J', 36, '2024-01-14', 0),
+(38, '012345678', 36, '2024-01-14', 1),
+(39, '123456789', 36, '2024-01-13', 1),
+(40, '456789012', 36, '2024-01-12', 0),
+(41, '987654321', 36, '2024-01-14', 1);
 
 --
 -- Disparadores `solicitudes`
@@ -281,7 +298,7 @@ ALTER TABLE `cursos`
 -- AUTO_INCREMENT de la tabla `solicitudes`
 --
 ALTER TABLE `solicitudes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- Restricciones para tablas volcadas
